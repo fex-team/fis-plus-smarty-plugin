bde859d1e6cb5dd4176d7198386f07e304935f06
tianlili <tianlili@baidu.com>
2013-11-04 13:18:17 +0800

    [change]增加iknow监控

--- test/performance/release.sh
+++ test/performance/release.sh
@@ -3,20 +3,16 @@
 TEST_PATH=/home/work/repos/fis-plus-smarty-plugin/test/performance
 cd ${TEST_PATH}
 
-BATMAN_CODE_PATH=${TEST_PATH}/product_code/batman
-BATMAN_OUTPUT_PATH=${TEST_PATH}/product_output/batman
-BATMAN_MODULES=(transit place common index addr feedback drive walk)
-	
-#batman
-rm -rf ${BATMAN_OUTPUT_PATH}
-for module in ${BATMAN_MODULES[@]} 
-do
-	cd ${BATMAN_CODE_PATH}/$module 
-	fisp release -d ${BATMAN_OUTPUT_PATH} --no-color
-done
+IKNOW_CODE_PATH=${TEST_PATH}/product_code/iknow
+IKNOW_OUTPUT_PATH=${TEST_PATH}/product_output/iknow
 
-fisp server install pc --root ${BATMAN_OUTPUT_PATH}
+#iknow
+rm -rf ${IKNOW_OUTPUT_PATH}
+cd ${IKNOW_CODE_PATH}
+fisp release -d ${IKNOW_OUTPUT_PATH} --no-color
+
+fisp server install pc --root ${IKNOW_OUTPUT_PATH}
 
 cd ${TEST_PATH}
-cp ../../*.*.php ${BATMAN_OUTPUT_PATH}/plugin
+cp ../../*.*.php ${IKNOW_OUTPUT_PATH}/plugin
 
