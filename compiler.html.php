410f74a986aa7d8c15b2c552556a4883b86b077e
zhangqian <zhangqian@imakejoy.com>
2016-08-03 23:12:55 +0800

    修复 PHP 执行为常驻进程时，一些变量需要销毁等问题

--- compiler.html.php
+++ compiler.html.php
@@ -18,6 +18,14 @@ function smarty_compiler_html($arrParams,  $smarty){
             $strAttr .= ' ' . $_key . '="<?php echo ' . $_value . ';?>"';
         }
     }
+    /**
+     * 后端的服务器进程为常住进程，需要在页面头部取消上次 register 的事件，防止在 smarty.fetch 方法调用 ，导致清空临时资源数据
+     * Date: 2016-07-21
+     * Author: raisezhang@hotmail.com
+     */
+    $strCode = '<?php ';
+    $strCode .= '$_smarty_tpl->unregisterFilter(\'output\', array(\'FISResource\', \'renderResponse\'));';
+    $strCode .= '?>';
 
     return $strCode . "<html{$strAttr}>";
 }
